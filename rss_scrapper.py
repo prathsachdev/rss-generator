365661dc16657ec57afa5e59ce49040c1ad02c50
Pratham Sachdev <94130756+prathsachdev@users.noreply.github.com>
2026-04-04 13:41:34 +0530

    update files

--- rss_scrapper.py
+++ rss_scrapper.py
@@ -1,15 +1,17 @@
-import requests
 from bs4 import BeautifulSoup
 from email.utils import format_datetime
 from datetime import datetime
-from xml.etree.ElementTree import Element, SubElement, ElementTree
+from xml.etree.ElementTree import Element, SubElement, ElementTree, register_namespace
 import cloudscraper
 
 URL = "https://idrw.org"
 
 # Scraping
 scraper = cloudscraper.create_scraper(
-    browser={"browser": "chrome", "platform": "windows"}
+    browser = {
+        "browser": "chrome", 
+        "platform": "windows"
+    }
 )
 
 html = scraper.get(URL, timeout=10).text
@@ -31,37 +33,81 @@ for article in articles:
     desc_element = article.select_one("div.art-postcontent > p:nth-child(3)")
     desc = desc_element.get_text(strip=True)
     
-    img_element = article.select_one("img")
-    img = img_element.get("data-src")
-    
     date_element = article.select_one("span.entry-date.updated")
     date = format_datetime(
                 datetime.strptime(date_element.get_text(strip=True), "%B %d, %Y").replace(hour=12)
             )
     
+    author_element = article.select_one("span.author.vcard > a")
+    author = author_element.get_text()
+    
+    img_element = article.select_one("img")
+    img = img_element.get("data-src")
+    
     items.append({
         "title": title,
         "link": link,
-        "image": img,
         "description": desc,
-        "pubDate": date
+        "pubDate": date,
+        "author": author,
+        "image": img,
     })
 
 # Generating RSS
-rss = Element("rss", version="2.0")
+rss = Element("rss", {
+    "version": "2.0"
+    # "xmlns:media": "http://search.yahoo.com/mrss/",
+    # "xmlns:atom": "http://www.w3.org/2005/Atom",
+    # "xmlns:dc": "http://purl.org/dc/elements/1.1/"
+})
+
+register_namespace("media", "http://search.yahoo.com/mrss/")
+register_namespace("atom", "http://www.w3.org/2005/Atom")
+register_namespace("dc", "http://purl.org/dc/elements/1.1/")
+
 channel = SubElement(rss, "channel")
 
 SubElement(channel, "title").text = "Indian Defence Research Wing"
 SubElement(channel, "link").text = URL
 SubElement(channel, "description").text = "INDIAN DEFENCE NEWS, LATEST DEFENCE NEWS, INDIAN ARMY, INDIAN AIR FORCE, INDIAN NAVY, DEFENSE NEWS"
+SubElement(channel, "{http://www.w3.org/2005/Atom}link", {
+    "href": URL,
+    "rel": "self",
+    "type": "application/rss+xml"
+})
+
+SubElement(channel, "generator").text = "GitHub Actions"
+
+image = SubElement(channel, "image")
+
+SubElement(image, "link").text = URL
+SubElement(image, "url").text = "https://www.google.com/s2/favicons?domain=https://idrw.org"
+SubElement(image, "title").text = "Indian Defence Research Wing"
 
 for item in items:
     entry = SubElement(channel, "item")
 
-    SubElement(entry, "title").text = item["title"]
-    SubElement(entry, "link").text = item["link"]
-    SubElement(entry, "description").text = item["description"]
-    SubElement(entry, "pubDate").text = item["pubDate"]
-    SubElement(entry, "guid").text = item["link"]
+    title = SubElement(entry, "title")
+    title.text = item["title"]
+    
+    link = SubElement(entry, "link")
+    link.text = item["link"]
+    
+    description = SubElement(entry, "description")
+    description.text = item["description"]
+    
+    pubDate = SubElement(entry, "pubDate")
+    pubDate.text = item["pubDate"]
+    
+    author = SubElement(entry, "{http://purl.org/dc/elements/1.1/}creator")
+    author.text = item["author"]
+    
+    image = SubElement(entry, "{http://search.yahoo.com/mrss/}content")
+    image.set("url", item["image"])
+    image.set("medium", "image")
+    
+    guid = SubElement(entry, "guid")
+    guid.set("isPermalink", "false")
+    guid.text = item["link"]
 
 ElementTree(rss).write("idrw.xml", encoding="utf-8", xml_declaration=True)
