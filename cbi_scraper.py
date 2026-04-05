a083df0977134d93020b58fe417330f4676b589e
Pratham Sachdev <94130756+prathsachdev@users.noreply.github.com>
2026-04-05 20:11:42 +0530

    add .xml files to feed

--- cbi_scraper.py
+++ cbi_scraper.py
@@ -5,115 +5,117 @@ from xml.etree.ElementTree import Element, SubElement, ElementTree, register_nam
 import cloudscraper
 from urllib.parse import urljoin
 
-URL = "https://chessbase.in"
+try:
+    URL = "https://chessbase.in"
 
-# Scraping
-scraper = cloudscraper.create_scraper(
-    browser = {
-        "browser": "chrome", 
-        "platform": "windows"
-    }
-)
+    # Scraping
+    scraper = cloudscraper.create_scraper(
+        browser = {
+            "browser": "chrome", 
+            "platform": "windows"
+        }
+    )
 
-html = scraper.get(URL, timeout=10).text
-soup = BeautifulSoup(html, "lxml")
+    html = scraper.get(URL, timeout=10).text
+    soup = BeautifulSoup(html, "lxml")
 
-print("Fetched length:", len(html))
+    print("Fetched length:", len(html))
 
-items = []
+    items = []
 
-articles = soup.select("article.newsitem")
-print("Articles found:", len(articles))
+    articles = soup.select("article.newsitem")
+    print("Articles found:", len(articles))
 
-for article in articles:
-    now = datetime.now(timezone.utc)
-    
-    link_element = article.select_one("h1 > a")
-    link = urljoin(URL, link_element.get("href"))
-    
-    title = link_element.get_text(strip=True)
-    
-    desc_element = article.select_one("p.new-cms")
-    desc = desc_element.get_text(strip=True)
-    
-    date_element = article.select_one("span.date")
-    date = format_datetime(
-                datetime.strptime(date_element.get_text(strip=True), "%d/%m/%Y -").replace(hour=now.hour, minute=now.minute, second=now.second, tzinfo=timezone.utc)
-            )
-    
-    img_element = article.select_one("img")
-    img = img_element.get("src")
-    
-    items.append({
-        "title": title,
-        "link": link,
-        "description": desc,
-        "pubDate": date,
-        "image": img,
+    for article in articles:
+        link_element = article.select_one("h1 > a")
+        link = urljoin(URL, link_element.get("href"))
+        
+        title = link_element.get_text(strip=True)
+        
+        desc_element = article.select_one("p.new-cms")
+        desc = desc_element.get_text(strip=True)
+        
+        date_element = article.select_one("span.date")
+        date = format_datetime(
+                    datetime.strptime(date_element.get_text(strip=True), "%d/%m/%Y -").replace(hour=0, minute=0, second=0, tzinfo=timezone.utc)
+                )
+        
+        img_element = article.select_one("img")
+        img = img_element.get("src")
+        
+        items.append({
+            "title": title,
+            "link": link,
+            "description": desc,
+            "pubDate": date,
+            "image": img,
+        })
+
+    # Generating RSS
+    rss = Element("rss", {
+        "version": "2.0"
+        # "xmlns:media": "http://search.yahoo.com/mrss/",
+        # "xmlns:atom": "http://www.w3.org/2005/Atom",
+        # "xmlns:dc": "http://purl.org/dc/elements/1.1/"
     })
 
-# Generating RSS
-rss = Element("rss", {
-    "version": "2.0"
-    # "xmlns:media": "http://search.yahoo.com/mrss/",
-    # "xmlns:atom": "http://www.w3.org/2005/Atom",
-    # "xmlns:dc": "http://purl.org/dc/elements/1.1/"
-})
+    register_namespace("media", "http://search.yahoo.com/mrss/")
+    register_namespace("atom", "http://www.w3.org/2005/Atom")
+    register_namespace("dc", "http://purl.org/dc/elements/1.1/")
 
-register_namespace("media", "http://search.yahoo.com/mrss/")
-register_namespace("atom", "http://www.w3.org/2005/Atom")
-register_namespace("dc", "http://purl.org/dc/elements/1.1/")
+    channel = SubElement(rss, "channel")
 
-channel = SubElement(rss, "channel")
+    SubElement(channel, "title").text = "Home - ChessBase India"
+    SubElement(channel, "description").text = "Read every news about chess in detail with ChessBase India in English. Read English news related with Chess such as Tournament, Players, every report of the Match."
+    SubElement(channel, "link").text = URL
+    SubElement(channel, "{http://www.w3.org/2005/Atom}link", {
+        "href": URL,
+        "rel": "self",
+        "type": "application/rss+xml"
+    })
 
-SubElement(channel, "title").text = "Home - ChessBase India"
-SubElement(channel, "description").text = "Read every news about chess in detail with ChessBase India in English. Read English news related with Chess such as Tournament, Players, every report of the Match."
-SubElement(channel, "link").text = URL
-SubElement(channel, "{http://www.w3.org/2005/Atom}link", {
-    "href": URL,
-    "rel": "self",
-    "type": "application/rss+xml"
-})
+    SubElement(channel, "generator").text = "GitHub Actions"
 
-SubElement(channel, "generator").text = "GitHub Actions"
+    image = SubElement(channel, "image")
 
-image = SubElement(channel, "image")
+    SubElement(image, "link").text = URL
+    SubElement(image, "url").text = "https://www.google.com/s2/favicons?domain=https://chessbase.in"
+    SubElement(image, "title").text = "Home - ChessBase India"
 
-SubElement(image, "link").text = URL
-SubElement(image, "url").text = "https://www.google.com/s2/favicons?domain=https://chessbase.in"
-SubElement(image, "title").text = "Home - ChessBase India"
+    for item in items:
+        entry = SubElement(channel, "item")
+        
+        if item.get("title"):
+            title = SubElement(entry, "title")
+            title.text = item["title"]
+        
+        if item.get("link"):
+            link = SubElement(entry, "link")
+            link.text = item["link"]
+        
+        if item.get("description"):
+            description = SubElement(entry, "description")
+            description.text = item["description"]
+        
+        if item.get("pubDate"):
+            pubDate = SubElement(entry, "pubDate")
+            pubDate.text = item["pubDate"]
+        
+        if item.get("author"):
+            author = SubElement(entry, "{http://purl.org/dc/elements/1.1/}creator")
+            author.text = item["author"]
+        
+        if item.get("image"):
+            image = SubElement(entry, "{http://search.yahoo.com/mrss/}content")
+            image.set("url", item["image"])
+            image.set("medium", "image")
+        
+        if item.get("link"):
+            guid = SubElement(entry, "guid")
+            guid.set("isPermalink", "true")
+            guid.text = item["link"]
 
-for item in items:
-    entry = SubElement(channel, "item")
-    
-    if item.get("title"):
-        title = SubElement(entry, "title")
-        title.text = item["title"]
-    
-    if item.get("link"):
-        link = SubElement(entry, "link")
-        link.text = item["link"]
-    
-    if item.get("description"):
-        description = SubElement(entry, "description")
-        description.text = item["description"]
-    
-    if item.get("pubDate"):
-        pubDate = SubElement(entry, "pubDate")
-        pubDate.text = item["pubDate"]
-    
-    if item.get("author"):
-        author = SubElement(entry, "{http://purl.org/dc/elements/1.1/}creator")
-        author.text = item["author"]
-    
-    if item.get("image"):
-        image = SubElement(entry, "{http://search.yahoo.com/mrss/}content")
-        image.set("url", item["image"])
-        image.set("medium", "image")
-    
-    if item.get("link"):
-        guid = SubElement(entry, "guid")
-        guid.set("isPermalink", "true")
-        guid.text = item["link"]
-
-ElementTree(rss).write("cbi.xml", encoding="utf-8", xml_declaration=True)
+    ElementTree(rss).write("feed/cbi.xml", encoding="utf-8", xml_declaration=True)
+except Exception as e:
+    print("Exception in cbi_scraper")
+    print(e)
